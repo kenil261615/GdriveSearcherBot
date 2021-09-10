c22e056562f221be741f094cd7caa3df70210e3a
kenil261615 <68728138+kenil261615@users.noreply.github.com>
2021-09-10 10:27:33 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,31 @@
+BOT_TOKEN = "1916685345:AAG2WamKztlaI_e5WIoHgdNzUOsVW8dBYgE"
+RESULTS_COUNT = 5  # NOTE Number of results to show, 4 is better
+SUDO_CHATS_ID = [-1001368224697]
+
+
+DRIVE_NAME = [
+    "Streamflix",  # folder 1 name
+    "HenilDrive",  # folder 2 name
+    "Course",  # folder 3 name
+    "Movies",  # ....
+    "Series",  # ......
+    "Others"  # and soo onnnn folder n names
+]
+
+DRIVE_ID = [
+    "0AFa_egrMicJyUk9PVA",  # folder 1 id
+    "0AOv_aRk8wDQaUk9PVA",  # folder 2 id
+    "11nZcObsJJHojHYg43dBS0_eVvJrSD7Nf",  # and so onn... folder n id
+    "10_hTMK8HE8k144wOTth_3x1hC2kZL-LR",
+    "1-oTctBpyFcydDNiptLL09Enwte0dClCq",
+    "1B9A3QqQqF31IuW2om3Qhr-wkiVLloxw8"
+]
+
+INDEX_URL = [
+    "https://streamflixs.tk/0:",  # folder 1 index link
+    "https://henilreleases.gq/0:",  # folder 2 index link
+    "https://dl.null.tech/0:/Course",  # and soo on folder n link
+    "https://dl.null.tech/0:/MOVIES",
+    "https://dl.null.tech/0:/Series",
+    "https://dl.null.tech/0:/Roms"
+]
